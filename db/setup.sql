7f3ed6de936511ec7f0935f3856913249d38bf97
jaketysome <jaketysome@gmail.com>
2025-02-17 13:28:07 +0000

    creates db setup

---
+++ db/setup.sql
@@ -0,0 +1,7 @@
+DROP DATABASE IF EXISTS wipeout_ship_dealer_test;
+
+DROP DATABASE IF EXISTS wipeout_ship_dealer;
+
+CREATE DATABASE wipeout_ship_dealer_test;
+
+CREATE DATABASE wipeout_ship_dealer;
