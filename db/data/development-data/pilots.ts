294c5e2232d14f9e2df6bcea964f6be6f0878285
jaketysome <jaketysome@gmail.com>
2025-02-17 13:03:24 +0000

    creates pilots development data

---
+++ db/data/development-data/pilots.ts
@@ -0,0 +1,102 @@
+export default [
+  {
+    fullName: "Sofia de la Rente",
+    slug: "sofia-de-la-rente",
+    teamId: 1,
+    yearOfBirth: 2030,
+    sex: "Female",
+    nationality: "French",
+    description:
+      "Sofia de la Rente (2030 - unknown) was a pilot for FEISAR during the F3600 League. She is also famous for becoming the first woman to fly non-stop around the world in a single-seater AG craft.",
+  },
+  {
+    fullName: "Paul Jackson",
+    slug: "paul-jackson",
+    teamId: 1,
+    yearOfBirth: 2016,
+    sex: "Male",
+    nationality: "British",
+    description:
+      "Paul Jackson (2016 - unknown) was a pilot for FEISAR during the F3600 League. Not much is known about him, suggesting that his data was considered top secret by FEISAR.",
+  },
+  {
+    fullName: "Carlos Beneto",
+    slug: "carlos-beneto",
+    teamId: 1,
+    yearOfBirth: 2048,
+    sex: "Male",
+    nationality: "Brazilian",
+    description:
+      "Carlos Beneto was a pilot for FEISAR during the F9000 League. Beneto was FEISAR's lead pilot for 15 years before he was pushed aside for Daniel Johnson. As a result, Beneto has become very bitter of his new teammate.",
+  },
+  {
+    fullName: "Daniel Johnson",
+    slug: "daniel-johnson",
+    teamId: 1,
+    yearOfBirth: 2053,
+    sex: "Male",
+    nationality: "British",
+    description:
+      "Daniel Johnson was a pilot for FEISAR during the F9000 League. He was FEISAR's most expensive signing in team history when he entered the F9000 in 2160, after which he was immediately promoted to the team's lead pilot at the expense of Carlos Beneto. He is notable for raising FEISAR's status as a leading team during the league.",
+  },
+  {
+    fullName: "John Dekka",
+    slug: "john-dekka",
+    teamId: 2,
+    yearOfBirth: 2014,
+    sex: "Male",
+    nationality: "American",
+    description:
+      "John Dekka (2014 - unknown) was a pilot for AG Systems during the F3600 League. He was one of AG Systems' best test pilots during the early years, before he became the team's lead pilot in the F3600 League.",
+  },
+  {
+    fullName: "Daniel Chang",
+    slug: "daniel-chang",
+    teamId: 2,
+    yearOfBirth: 2023,
+    sex: "Male",
+    nationality: "Chinese",
+    description:
+      "Daniel Chang (2023 - 2056) was a pilot for AG Systems during the F3600 League. It is mentioned that Chang defected from China when he was still a test pilot in the Communist government's program. After that, he eventually became one of AG Systems' pilots by registering himself to that team. He is also notable for becoming the first fatality in AG racing history.",
+  },
+  {
+    fullName: "Arial Tetsuo",
+    slug: "arial-tetsuo",
+    teamId: 3,
+    yearOfBirth: 2030,
+    sex: "Female",
+    nationality: "Japanese",
+    description:
+      "Arial Tetsuo was a pilot for Auricom during the F3600 League. After retiring from AG racing, she became one of the most influential politicians during the mid 21st century. She also had a fierce rivalry with her twin sister, Arian Tetsuo of Qirex.",
+  },
+  {
+    fullName: "Anastasia Cherovoski",
+    slug: "anastasia-cherovoski",
+    teamId: 3,
+    yearOfBirth: null,
+    sex: "Female",
+    nationality: null,
+    description:
+      "Anastasia Cherovoski was a pilot for Auricom during the F3600 League. Not much is known about her, although some details about her might be known by Qirex lead pilot Kel Solaar.",
+  },
+  {
+    fullName: "Kel Solaar",
+    slug: "kel-solaar",
+    teamId: 4,
+    yearOfBirth: 2012,
+    sex: "Male",
+    nationality: "Russian",
+    description:
+      "Kel Solaar was a pilot for Qirex during the F3600 League. He is widely known to be one of the most successful pilots in AG racing history, winning more F3600 championships than all other pilots combined. This is not surprising, considering he was an authority on high speed filght and advanced weapons systems. He is also known for his mysterious relationship with Auricom's second pilot Anastasia Cherovoski.",
+  },
+  {
+    fullName: "Arian Tetsuo",
+    slug: "arian-tetsuo",
+    teamId: 4,
+    yearOfBirth: 2030,
+    sex: "Female",
+    nationality: "Japanese",
+    description:
+      "Arian Tetsuo was a pilot for Qirex during the F3600 League. She also had a fierce rivalry with her twin sister, Arial Tetsuo of Auricom.",
+  },
+];
